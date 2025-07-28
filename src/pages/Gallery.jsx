8f151dafa0985d331930896c516d934ffb572bc8
Seenusharma <roshansharma7250@gmail.com>
2025-07-28 19:16:19 +0530

    update gallery section

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import GalleryHead from '../components/GalleryHead'
+import React from "react";
+import GalleryHead from "../components/GalleryHead";
+import CircularGallery from "../components/CircularGallery ";
 
 const Gallery = () => {
   return (
     <div>
-      <GalleryHead/>
+      <div style={{ height: "600px", position: "relative" }}>
+        <CircularGallery
+          bend={3}
+          textColor="#ffffff"
+          borderRadius={0.05}
+          scrollEase={0.02}
+        />
+      </div>
+      <GalleryHead />
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
